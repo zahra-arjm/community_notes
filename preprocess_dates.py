a3edee512df1e6e34677d81104398c724130de88
Zahra <z.arjmandi@gmail.com>
2026-07-19 10:08:50 +0330

    updated a bit to accomodate for merged notes from 2026

--- preprocess_dates.py
+++ preprocess_dates.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from datetime import datetime
 
-with open('./notes-00000.tsv', 'r') as n:
-    notes = pd.read_csv(n, sep='\t')
+# with open('./notes-00000.tsv', 'r') as n:
+#     notes = pd.read_csv(n, sep='\t')
 
+# in 2026 we had multiple notes and I merged them into 1 parquet file
+notes = pd.read_parquet('./data2026proc/notes.parquet', engine='auto')
 
 # create a column with datetime format
 notes['createdAt'] = pd.to_datetime(notes['createdAtMillis'], unit='ms')
@@ -13,4 +15,4 @@ notes['createdAtYear'] = notes['createdAt'].dt.year
 notes['createdAtMonth'] = notes['createdAt'].dt.month
 
 # save in parquet format
-notes.to_parquet('notes.parquet')
+notes.to_parquet('./data2026proc/notes.parquet')
